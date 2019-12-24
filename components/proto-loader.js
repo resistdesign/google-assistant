4ed546f19922a80d7772cbf718fcdcabf4348dc5
Ryan Graff <ryantgraff@resistdesign.com>
2019-12-24 15:49:23 -0500

    feat: (proto-loader) Add a global config for the proto directory.

--- components/proto-loader.js
+++ components/proto-loader.js
@@ -1,10 +1,18 @@
 const path = require('path');
 const protobuf = require('protobufjs');
+const GAGlobals = require('./ga-globals');
 
 const protoRoot = new protobuf.Root();
 protoRoot.resolvePath = (origin, target) => {
+  const {
+    PROTO_BUFFER_DIRECTORY
+  } = GAGlobals;
+  const baseDir = !!PROTO_BUFFER_DIRECTORY ?
+    PROTO_BUFFER_DIRECTORY :
+    path.join(__dirname, '../lib');
+
   // we need to resolve each import to the lib directory instead of this one
-  return protobuf.util.path.resolve(origin, path.resolve(__dirname, '../lib', target));
+  return protobuf.util.path.resolve(origin, path.resolve(baseDir, target));
 };
 
 module.exports = protoRoot;
